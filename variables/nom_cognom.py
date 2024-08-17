a50b30eccd00e57fc94e537ad7640e42e42f7dfb
Marc <m.roset.bermudez@gmail.com>
2024-08-17 20:57:41 +0200

    Ejercicio de nombre y apellido

---
+++ variables/nom_cognom.py
@@ -0,0 +1,7 @@
+#Este programa pide el nombre y el apellido del usuario y le muestra un mensaje amistoso
+#NO CONTROLA VALORES ERRONEOS
+
+nom = input("Nom?\n")
+cognom = input("Cognom?\n")
+
+print(f"Ei {nom}, tinc un amic que també es diu {cognom}!")
